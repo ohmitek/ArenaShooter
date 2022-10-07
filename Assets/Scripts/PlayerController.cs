fbc22eb72877c5bd788bcd0e7a23bb7caef52745
KristTalv <krista.talvipuro@edu.turkuamk.fi>
2022-10-07 10:38:43 +0300

    Commiting changes in MainSecene: PlayerScript addet to Player object. When playing, you should get "Testi onnistunut!" message. Also deletet the SampelScene from Assets.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Testi_Onnistunut");
     }
 
     // Update is called once per frame

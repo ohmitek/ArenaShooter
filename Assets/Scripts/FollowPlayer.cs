ca49ef29d65bcbfd85de3479998402fe6de1b26e
KristTalv <krista.talvipuro@edu.turkuamk.fi>
2022-10-10 10:21:17 +0300

    - New scripts (Follow Player): Camera follows the player now -Changes to PlayerController script: Player can now move -Cahnges to scene: FolowPlayer script is addet to Camera in the scene

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject playerObject;
+    private Vector3 offset = new Vector3(0, 3, 0);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = playerObject.transform.position + offset;
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float horizontalInput;
+    private float verticalInput;
+
+    public float playerSpeed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Testi_Onnistunut");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Making player input available.
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Makes Player game object move acros the arena.
+        transform.Translate(Vector3.up * verticalInput * Time.deltaTime * playerSpeed);
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerSpeed);
+
     }
 }

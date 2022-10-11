f3e98507887170cf8e0e02fcd5cede0bc9993eab
DESKTOP-G3ATE9P\Superjamppa <teemu.homi@netikka.fi>
2022-10-11 16:57:58 +0300

    Added and customized .gitignore file to project root folder! Added Grass_image to game. Added Konsepti_4 (player image) to game.
    Added Spawn_PointOne object to project.
    Added Spawn_PointTwo object to project.
    Added Spawn_PointThree object to project.
    Added Spawn_PointFour object to project.
    
    Added Enemy_One to project.
    Added Enemy_Two to project.
    
    Added SpawnManager script to project.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject Spawn_PointOne;
+    public GameObject Spawn_PointTwo;
+    public GameObject Spawn_PointThree;
+    public GameObject Spawn_PointFour;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

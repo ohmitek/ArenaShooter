f935af728ac42f4a6d16512d447753301297f07c
DESKTOP-G3ATE9P\Superjamppa <teemu.homi@netikka.fi>
2022-11-09 22:08:58 +0200

    Enemies die when they touch the Bullet. + EnemyDeath.cs to Enemy. + Tag "Bullet" to Bullet. + Tag "Enemy" to Enemy. + Sorting Layer settings Game and Order 1 to Bullet and Enemy.

---
+++ Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        
+        if (col.gameObject.CompareTag ("Bullet"))
+        {
+            
+            Debug.Log ("Collision detected!");
+            Destroy (col.gameObject);
+            Destroy (gameObject);
+        }
+
+        
+    }
+}
